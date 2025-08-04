3b6727ab10ab4f7dc579e955c5bda6d018b78ba6
Dirico19 <diego090819@gmail.com>
2025-08-03 23:28:27 -0500

    add validator

---
+++ Fuxion.Ali.Contracts/Helpers/ValidatorHelper.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+
+namespace Fuxion.Ali.Contracts.Helpers
+{
+    public static class ValidatorHelper
+    {
+        public static Func<object, string, Task<IEnumerable<string>>> ValidateValue<T>(this IValidator<T> validator) where T : class
+        {
+            return async (model, propertyName) =>
+            {
+                if (model is not T castModel)
+                    return [$"Tipo de modelo inválido: se esperaba {typeof(T).Name}, se recibió {model?.GetType().Name}"];
+
+                var context = ValidationContext<T>.CreateWithOptions(castModel, opts =>
+                    opts.IncludeProperties(propertyName));
+
+                ValidationResult result = await validator.ValidateAsync(context);
+
+                return result.IsValid
+                    ? []
+                    : result.Errors.Select(e => e.ErrorMessage);
+            };
+        }
+    }
+}
